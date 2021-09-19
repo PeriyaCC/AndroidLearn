0009158d1037520be378b26443629a52b57884e5
Periya <periyanayagam.s@cognitiveclouds.com>
2021-09-19 22:58:31 +0530

    Added sample dialog validation

--- app/src/main/java/com/cc/androidlearn/LauncherActivity.kt
+++ app/src/main/java/com/cc/androidlearn/LauncherActivity.kt
@@ -7,7 +7,9 @@ import android.os.Bundle
 import android.view.Window
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_launcher.*
 
 
@@ -24,7 +26,7 @@ class LauncherActivity : AppCompatActivity() {
     private fun initView() {
         rvAndroidVersions.apply {
             layoutManager = LinearLayoutManager(this@LauncherActivity)
-            adapter = VersionsAdapter(::onItemClicked,::showDeleteAlert)
+            adapter = VersionsAdapter(::onItemClicked, ::showDeleteAlert)
         }
     }
 
@@ -32,22 +34,32 @@ class LauncherActivity : AppCompatActivity() {
 
     }
 
-    private fun onItemClicked(selectedItem : String?){
+    private fun onItemClicked(selectedItem: String?) {
         Toast.makeText(this, selectedItem, Toast.LENGTH_SHORT).show()
     }
 
     private fun loadData() {
-        (rvAndroidVersions.adapter as VersionsAdapter).versionListItems = DataProvider.getAndroidVersions()
+        (rvAndroidVersions.adapter as VersionsAdapter).versionListItems =
+            DataProvider.getAndroidVersions()
     }
 
-    private fun showDeleteAlert(deleteItemPosition : Int){
-       //write code to show alert
+    private fun showDeleteAlert(deleteItemPosition: Int) {
+        //write code to show alert
     }
 
-        private fun showCustomDialog(){
+    private fun showCustomDialog() {
         with(Dialog(this)) {
             requestWindowFeature(Window.FEATURE_NO_TITLE)
             setContentView(R.layout.item_dialog)
+            val etSample = findViewById<TextInputEditText>(R.id.etSample)
+
+            findViewById<AppCompatButton>(R.id.btnAdd).setOnClickListener {
+                if(etSample.text.toString().trim().isEmpty()){
+                    etSample.error = "Sample is required" // change message as per the field!
+                }else
+                    dismiss()
+            }
+
             window?.apply {
                 setSoftInputMode(android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
                 setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
